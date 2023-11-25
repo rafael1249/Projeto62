b7bb2c1ec9942ac111c7c4ab10cc04fe1a2bbe70
Rafael dos Anjos <89105388+rafael1249@users.noreply.github.com>
2023-11-25 17:02:37 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+ import * as firebase from "firebase";
+ var firebaseConfig = {
+  apiKey: "AIzaSyDTpuZZ2otpKB4BoeYEe6LQ20aySqOzLHA",
+  authDomain: "projeto60-de1ec.firebaseapp.com",
+  databaseURL: "https://projeto60-de1ec-default-rtdb.firebaseio.com",
+  projectId: "projeto60-de1ec",
+  storageBucket: "projeto60-de1ec.appspot.com",
+  messagingSenderId: "13674294458",
+  appId: "1:13674294458:web:4aa6b0a5bfc6d8490c6683"
+};
+  export default firebase.database()
+ 
+
+  
